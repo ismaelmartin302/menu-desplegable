2037ee4a4b8ab9a046f27d2ed23071d902d7af45
isma <ismaelmartin302@gmail.com>
2025-01-22 17:20:09 +0100

    Mobile view created

--- menu-desplegable.php
+++ menu-desplegable.php
@@ -10,13 +10,26 @@ function show_custom_menu() {
 
     return '
     <nav class="menu">
-        <ul class="menu__list">
-            <img class="menu__logo" src="' . plugins_url('images/logo.png', __FILE__) . '" alt="Logo">
-            <li class="menu__item">Formación</li>
-            <li class="menu__item">Somos KS</li>
-            <li class="menu__item">KS Talent</li>
-            <li class="menu__item">Blog</li>
-        </ul>
+        <div class="menu__group">
+            <img class="menu__logo" src="' . plugins_url('images/logo.png', __FILE__) . '" alt="Logo"></li>
+            <ul class="menu__list">
+                <li class="menu__item">Formación</li>
+                <li class="menu__item">Somos KS</li>
+                <li class="menu__item">KS Talent</li>
+                <li class="menu__item">Blog</li>
+
+            </ul>
+            <button class="menu__toggle" id="menuToggle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+            </button>
+            <button class="menu__close" id="menuClose">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                    </svg>
+            </button>
+        </div>
         <ul class="menu__list">
             <li class="menu__item">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16">
